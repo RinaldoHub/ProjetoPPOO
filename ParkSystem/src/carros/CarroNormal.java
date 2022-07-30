6065b133386c4f6f5e4c6afd3b49f84beb766c59
Lucas monteiro <lucas.monteiro@liferay.com>
2022-07-30 14:11:59 -0300

    Removendo redundancia

--- ParkSystem/src/carros/CarroNormal.java
+++ ParkSystem/src/carros/CarroNormal.java
@@ -4,9 +4,9 @@ import vagas.VagaNormal;
 
 public class CarroNormal extends Carro {
     VagaNormal vaga;
-    public boolean isIdoso = false;
-    public boolean isPCD = false;
-    
+    public boolean isIdoso;
+    public boolean isPCD;
+
     public CarroNormal(String placa) {
         super(placa);
         this.isIdoso = false;
