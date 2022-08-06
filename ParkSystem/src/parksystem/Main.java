a15ae0f6ab5bd29d303f85ebd9280ef64f732c76
Lucas monteiro <lucas.monteiro@liferay.com>
2022-08-06 12:42:17 -0300

    Reparo na validação da possibilidade de um estacionar

--- ParkSystem/src/parksystem/Main.java
+++ ParkSystem/src/parksystem/Main.java
@@ -11,8 +11,6 @@ public class Main
         //IMPLEMENTAR A CRIAÇÃO COM FLYWEIGHT
         //Aqui é a parte para testar a criação de objetos
         Carro carro1 = new CarroNormal("AND1337");
-        //Carro carro2 = new CarroIdoso("RFB2303");
-        //Carro carro3 = new CarroPCD("LUC1234");
 
         /* SINGLETON */
         //Implementação do SINGLETON, criando apenas UM objeto do seu tipo
@@ -20,20 +18,12 @@ public class Main
                 
         /* TESTE DE LINKAGEM ENTRE OBJETOS CARRO-VAGA */
         //setando os carros às vagas
-        park.estacionar(carro1, "a1"); //o estacionamento que DEVE OBSERVAR SE UM CARRO FOI
-        //park.estacionar(carro2, a1); //ESTACIONADO
-        //park.estacionar(carro3, c4);
+        //park.estacionar(carro1, "c1"); //O estacionamento que DEVE OBSERVAR SE UM CARRO FOI
+        park.estacionar(carro1, "a1"); //ESTACIONADO
         
-        System.out.println("///// Dados do carro /////");
-        System.out.println("Placa: " + carro1.getPlaca());
-        System.out.println("Estacionado? " + carro1.estacionado);
-        System.out.println("Vaga: " + carro1.getVaga());
-        System.out.println("Idoso? " + carro1.isIdoso);
-        System.out.println("PCD? " + carro1.isPCD);
+        carro1.getAtributos();
         
         //deslinkando os carros às vagas
         park.sair(carro1);
-        //park.sair(a2);
-        //park.sair(a3);
     }
 }
