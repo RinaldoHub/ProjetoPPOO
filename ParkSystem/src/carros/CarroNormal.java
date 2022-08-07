4aa247fee2815f2e0742c3d32d24e2458a31678d
Lucas monteiro <lucas.monteiro@liferay.com>
2022-08-07 15:33:00 -0300

    Diminuição nas redundâncias

--- ParkSystem/src/carros/CarroNormal.java
+++ ParkSystem/src/carros/CarroNormal.java
@@ -4,8 +4,6 @@ import vagas.VagaNormal;
 
 public class CarroNormal extends Carro {
     VagaNormal vaga;
-    public boolean isIdoso;
-    public boolean isPCD;
 
     public CarroNormal(String placa) {
         super(placa);
